f8c1a93c12aa815f23740949acafde0328866ce6
Ray Zane <raymondzane@gmail.com>
2021-08-10 20:36:15 -0400

    Build a PostgreSQL adapter

--- lib/database_cursor.rb
+++ lib/database_cursor.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
 require_relative "database_cursor/version"
+require_relative "database_cursor/postgresql_adapter"
 
 module DatabaseCursor
   class Error < StandardError; end
-  # Your code goes here...
+
+  ADAPTERS = {
+    "postgresql" => PostgreSQLAdapter
+  }
+
+  module QueryMethods
+    def cursor(...)
+      adapter = ADAPTERS.fetch(connection_config[:adapter])
+      adapter.new(all, ...)
+    rescue KeyError => error
+      raise "Unknown adapter: #{error.key}"
+    end
+  end
 end
---
+++ lib/database_cursor/abstract_adapter.rb
@@ -0,0 +1,44 @@
+module DatabaseCursor
+  class Adapter
+    # Create a new cursor
+    # @param relation [ActiveRecord::Relation]
+    # @param batch_size [Integer]
+    def initialize(relation, batch_size: 1_000)
+      @relation = relation
+      @batch_size = batch_size
+      @name = "cursor_#{SecureRandom.uuid.tr("-", "_")}"
+    end
+
+    private
+
+    attr_reader :relation, :batch_size, :name
+
+    delegate :model, :connection, to: :relation
+
+    def sql
+      connection.unprepared_statement { relation.to_sql }
+    end
+
+    def open
+      connection.execute "DECLARE #{name} NO SCROLL CURSOR FOR #{sql}"
+    end
+
+    def close
+      connection.execute "CLOSE #{name}"
+    end
+
+    def fetch
+      model.find_by_sql "FETCH #{batch_size} FROM #{name}"
+    end
+
+    def fetch_rows
+      result = connection.execute "FETCH #{batch_size} FROM #{name}"
+      result.to_a
+    end
+
+    def fetch_tuples
+      result = connection.execute "FETCH #{batch_size} FROM #{name}"
+      result.values
+    end
+  end
+end
