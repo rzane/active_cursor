16b24a891fc955f4974e737d6fa6c4dae9941c32
Ray Zane <raymondzane@gmail.com>
2021-08-10 22:32:15 -0400

    Do less in the abstract class

--- lib/database_cursor/abstract_adapter.rb
+++ lib/database_cursor/abstract_adapter.rb
@@ -2,77 +2,31 @@ require "active_support/core_ext/module/delegation"
 
 module DatabaseCursor
   class AbstractAdapter
-    # Create a new cursor
-    # @param relation [ActiveRecord::Relation]
-    # @param batch_size [Integer]
     def initialize(relation, batch_size: 1_000)
       @relation = relation
       @batch_size = batch_size
-      @name = "cursor_#{SecureRandom.uuid.tr("-", "_")}"
     end
 
-    def each(&block)
-      iterate do
-        records = fetch_records
-        records.each(&block)
-        records.length
-      end
+    def each
+      raise NotImplementedError
     end
 
-    def each_row(&block)
-      iterate do
-        result = fetch_result
-        result.each(&block)
-        result.ntuples
-      end
+    def each_row
+      raise NotImplementedError
     end
 
-    def each_tuple(&block)
-      iterate do
-        result = fetch_result
-        result.values.each(&block)
-        result.ntuples
-      end
+    def each_tuple
+      raise NotImplementedError
     end
 
     private
 
-    attr_reader :relation, :batch_size, :name
+    attr_reader :relation, :batch_size
 
     delegate :model, :connection, to: :relation
 
     def sql
       connection.unprepared_statement { relation.to_sql }
-    end
-
-    def open
-      connection.execute "DECLARE #{name} NO SCROLL CURSOR FOR #{sql}"
-    end
-
-    def close
-      connection.execute "CLOSE #{name}"
-    end
-
-    def fetch_records
-      model.find_by_sql "FETCH #{batch_size} FROM #{name}"
-    end
-
-    def fetch_result
-      connection.execute "FETCH #{batch_size} FROM #{name}"
-    end
-
-    def iterate
-      connection.transaction do
-        open
-
-        begin
-          loop do
-            break if yield < batch_size
-          end
-        ensure
-          close
-        end
-      end
     end
   end
 end
