0481f108a69b1648107672fd7e2cd83a50de45b2
Ray Zane <raymondzane@gmail.com>
2021-08-10 20:18:56 -0400

    First commit

---
+++ spec/database_cursor_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe DatabaseCursor do
+  it "has a version number" do
+    expect(DatabaseCursor::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
